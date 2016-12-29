c8a6ebb8b2e0efa7eaaef245fdcbf37d7f08c7a1
Roberto Gibellini <gibellini.roberto@gmail.com>
2016-12-29 15:04:30 +0100

    n5120 : add missing RIL parameters
    Change-Id: I5469a31aa0efa9c5a5396820e9318fd8eb777f5f

--- n5120.mk
+++ n5120.mk
@@ -22,6 +22,12 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/fstab.smdk4x12:root/fstab.smdk4x12
 
+# RIL
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.telephony.default_network=9 \
+    telephony.lteOnGsmDevice=1 \
+    ro.ril.telephony.mqanelements=6
+
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
